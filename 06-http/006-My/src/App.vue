b9af16e3df73387301c475c75ccd4f84f4e5885e
Maksim.Bardovskii <m.bardovskii@skoltech.ru>
2026-01-03 14:08:27 +0300

    HTTP get post . Load functions buttons

--- 06-http/006-My/src/App.vue
+++ 06-http/006-My/src/App.vue
@@ -1,11 +1,80 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="container">
+    <form class="card" @submit.prevent="createPerson">
+      <h2>Работа с базой данных</h2>
+
+      <div class="form-control">
+        <label for="name">Введите имя</label>
+        <input type="text" id="name" v-model.trim="name">
+      </div>
+
+      <button class="btn primary" :disabled="name.length === 0">Создать человека</button>
+    </form>
+    <app-people-list 
+      :people="people"
+      @load="loadPeople"
+      @remove="removePerson"
+      ></app-people-list>
+  </div>
 </template>
 
-<style scoped></style>
+<script>
+import AppPeopleList from './components/AppPeopleList.vue';
+import axios from 'axios';
+
+export default {
+  data() {
+    return {
+      name: '',
+      people: []
+    }
+  },
+  mounted() {
+    this.loadPeople()
+  },
+  components: {
+    AppPeopleList
+  },
+  methods: {
+    async createPerson() {
+      // https://vue-with-https-006-default-rtdb.europe-west1.firebasedatabase.app/people.json
+      //this.name
+      const response = await fetch('https://vue-with-https-006-default-rtdb.europe-west1.firebasedatabase.app/people.json', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          firstName: this.name
+        })
+      })
+
+      const firebaseData = await response.json()
+
+      console.log(firebaseData);
+      this.people.push({
+        firstName: this.name,
+        id: firebaseData.name
+      })
+      this.name = ''
+    },
+    async loadPeople() {
+      const {data}= await axios.get('https://vue-with-https-006-default-rtdb.europe-west1.firebasedatabase.app/people.json')
+      this.people = Object.keys(data).map(key => {
+        return {
+          id: key,
+          ...data[key] // firstName: data[key]
+        }
+      })
+    },
+    async removePerson(id) {
+      await axios.delete(`https://vue-with-https-006-default-rtdb.europe-west1.firebasedatabase.app/people/${id}.json`)
+      this.people = this.people.filter(person => person.id !== id)
+    }
+  }
+}
+</script>
+
+<style>
+
+</style>
