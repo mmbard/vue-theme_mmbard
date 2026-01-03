2aa91d60f2f2602fcbfedb8583a722570160e3aa
Maksim.Bardovskii <m.bardovskii@skoltech.ru>
2026-01-03 12:21:59 +0300

    Practice 5 done

---
+++ 05-forms/05-forms-my/src/components/AppInput.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="form-control" :class="{invalid: error}">
+        <label :for="id">{{ label }}</label>
+        <input 
+            type="text" 
+            :id="id" 
+            :placeholder="placeholder" 
+            v-model.trim="modelValue"
+            @input="change" 
+        />
+        <small v-if="error">{{ error }}</small>
+    </div>
+</template>
+
+<script>
+export default {
+    emits: ['update:value'],
+    props: {
+        modelValue: String,
+        placeholder: String,
+        label: String,
+        error: String 
+    },
+    methods: {
+        change(event) {
+            this.$emit('update:value', event.target.value)
+        }
+    },
+    data() {
+        return {
+            id: 'input-' + Math.random()
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
